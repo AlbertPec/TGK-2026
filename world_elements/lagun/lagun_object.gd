aa0fd38d95f3ce15e48c78a017e97b595a633f45
Kinga Bunkowska <kingabunkowska@gmail.com>
2026-06-11 03:27:28 +0200

    Add fadeout endscreen

--- world_elements/lagun/lagun_object.gd
+++ world_elements/lagun/lagun_object.gd
@@ -2,14 +2,17 @@ extends Node2D
 
 @onready var interactable = $Interactable
 
+const FADEOUT_GROUP := "fadeout_effect"
+const LAGUN_MESSAGE := "To całkiem zwyczajny rogal"
+
 func _on_interacted(interactor = null):
-	GlobalSignals.emit_signal("change_textbox_text","To tylko croissant")
+	var fadeout_effect := get_tree().get_first_node_in_group(FADEOUT_GROUP)
+	if fadeout_effect != null and fadeout_effect.has_method("show_message"):
+		fadeout_effect.show_message(LAGUN_MESSAGE)
+		return
+
+	GlobalSignals.emit_signal("change_textbox_text", LAGUN_MESSAGE)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactable.connect("interacted", _on_interacted)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass

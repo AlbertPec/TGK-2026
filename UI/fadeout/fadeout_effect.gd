6a6ea0927629838f09111ceafd39adddf05da40b
Albert Pęciak <147136467+AlbertPec@users.noreply.github.com>
2026-06-17 20:30:09 +0200

    Revert "Npcs"

--- UI/fadeout/fadeout_effect.gd
+++ UI/fadeout/fadeout_effect.gd
@@ -1,16 +1,6 @@
 extends Node2D
 
 @onready var fade = $CanvasLayer/ColorRect
-@onready var message_label: Label = $CanvasLayer/MessageLabel
-
-const FADEOUT_GROUP := "fadeout_effect"
-
-var _message_sequence_running: bool = false
-
-func _ready() -> void:
-	if not is_in_group(FADEOUT_GROUP):
-		add_to_group(FADEOUT_GROUP)
-	message_label.visible = false
 
 
 func fade_in():
@@ -23,18 +13,3 @@ func fade_out():
 	var tween = create_tween()
 	tween.tween_property(fade, "modulate:a", 0.0, 1.5)
 	await tween.finished
-
-func show_message(message: String, display_time: float = 1.8) -> void:
-	if _message_sequence_running:
-		return
-
-	_message_sequence_running = true
-	message_label.text = message
-	message_label.visible = true
-
-	await fade_in()
-	await get_tree().create_timer(display_time).timeout
-	await fade_out()
-
-	message_label.visible = false
-	_message_sequence_running = false
--- world_elements/lagun/lagun_object.gd
+++ world_elements/lagun/lagun_object.gd
@@ -2,16 +2,8 @@ extends Node2D
 
 @onready var interactable = $Interactable
 
-const FADEOUT_GROUP := "fadeout_effect"
-const LAGUN_MESSAGE := "To całkiem zwyczajny rogal"
-
 func _on_interacted(interactor = null):
-	var fadeout_effect := get_tree().get_first_node_in_group(FADEOUT_GROUP)
-	if fadeout_effect != null and fadeout_effect.has_method("show_message"):
-		fadeout_effect.show_message(LAGUN_MESSAGE)
-		return
-
-	GlobalSignals.emit_signal("change_textbox_text", LAGUN_MESSAGE)
+	GlobalSignals.emit_signal("change_textbox_text","To tylko croissant")
 
 func _ready() -> void:
 	interactable.connect("interacted", _on_interacted)
